b5896d0743670e853f282bf4d92913de4683095d
Sidharth <sidharth2020sharma@gmail.com>
2024-10-19 22:33:09 +0530

    #

--- backend/Socket/socket.js
+++ backend/Socket/socket.js
@@ -5,11 +5,11 @@ import express from 'express';
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:['https://slrtech-chatapp.onrender.com'],
-        methods:["GET","POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: ["https://chatdemoapp1.onrender.com"],
+    methods: ["GET", "POST"],
+  },
 });
 
 export const getReciverSocketId = (receverId)=>{
